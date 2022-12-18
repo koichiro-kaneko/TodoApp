cd806e68a42b215961fd3bcd10ee1c7ea65ebed4
koichiro <koichiro.world@gmail.com>
2022-12-19 01:20:47 +0900

    test1

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <header>
+        <h1>Your TodoList</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Title;
